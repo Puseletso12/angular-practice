e31d77228148621c8eea2b718cb24d618eb15b2a
Puseletso Chuene <puseletsochuene88@gmail.com>
2024-08-23 13:36:35 +0200

    page-not-found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'users', component: UserListComponent },
+  {path: '**', component: PageNotFoundComponent}
+];
